70bac8a68e226633f95a296cc88fc09c7850f922
Lukas <lukas.hertel@tum.de>
2024-10-29 21:24:09 +0100

    cli: Added new binary crate for CLI application

---
+++ src/bin/cli/app.rs
@@ -0,0 +1,5 @@
+//! Stores persistant data of the CLI binary
+
+pub enum CurrentScreen {
+    Main
+}
---
+++ src/bin/cli/main.rs
@@ -0,0 +1,8 @@
+extern crate p2p_file_sync;
+
+mod ui;
+mod app;
+
+fn main() {
+    println!("Hello World!");
+}
